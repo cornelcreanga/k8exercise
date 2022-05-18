5754b4201c4adeb4dbc44d1644622d6997fef08f
Corneliu Creanga <corneliu.creanga@crowdstrike.com>
2022-05-18 20:41:59 +0300

    remove unused files/fix typos

--- docker-push.sh
+++ docker-push.sh
@@ -1,3 +1,4 @@
+mvn clean package
 docker build -f Dockerfile --tag k8exercise .
 docker tag k8exercise:latest cornelcreanga/k8exercise:3.1.1
-docker push .gitignorecornelcreanga/k8exercise:3.1.1
+docker push cornelcreanga/k8exercise:3.1.1
--- miniStart.sh
+++ miniStart.sh
@@ -1,9 +1,6 @@
-#!/usr/bin/zsh
-
 minikube start  --cpus 4  --memory 8192  --insecure-registry=192.168.0.0/16
 
 minikube addons enable metrics-server
 kubectl create -f namespaces.yaml
 helm repo add spark-operator https://googlecloudplatform.github.io/spark-on-k8s-operator
-helm install sparkoperator spark-operator/spark-operator --namespace spark-operator --set sparkJobNamespace=spark-apps,enableWebhook=true --set image.tag=v1beta2-1.3.3-3.1.1
-
+helm install sparkoperator spark-operator/spark-operator --namespace spark-operator --set sparkJobNamespace=spark-apps,enableWebhook=true --set image.tag=v1beta2-1.3.6-3.1.1
